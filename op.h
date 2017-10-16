6ba4aa29b5ae818eaa2f112655868109db058934
omotyliu <skkif1@gmail.com>
2017-10-16 18:06:31 +0300

    du

---
+++ op.h
@@ -0,0 +1,154 @@
+#ifndef COREWAR_OP_H
+#define COREWAR_OP_H
+
+#include <panel.h>
+#include "libft/ft_printf.h"
+
+
+#define IND_SIZE				2
+#define REG_SIZE				4
+#define DIR_SIZE				REG_SIZE
+
+
+# define REG_CODE				1
+# define DIR_CODE				2
+# define IND_CODE				3
+
+
+#define MAX_ARGS_NUMBER			4
+#define MAX_PLAYERS				4
+#define MEM_SIZE				(4*1024)
+#define IDX_MOD					(MEM_SIZE / 8)
+#define CHAMP_MAX_SIZE			(MEM_SIZE / 6)
+
+#define COMMENT_CHAR			'#'
+#define LABEL_CHAR				':'
+#define DIRECT_CHAR				'%'
+#define SEPARATOR_CHAR			','
+
+#define LABEL_CHARS				"abcdefghijklmnopqrstuvwxyz_0123456789"
+
+#define NAME_CMD_STRING			".name"
+#define COMMENT_CMD_STRING		".comment"
+
+#define REG_NUMBER				16
+
+#define CYCLE_TO_DIE			1536
+#define CYCLE_DELTA				50
+#define NBR_LIVE				21
+#define MAX_CHECKS				10
+#define MARGIN                  2 //custom def
+
+
+typedef char	t_arg_type;
+
+#define T_REG					1
+#define T_DIR					2
+#define T_IND					4
+#define T_LAB					8
+
+
+# define PROG_NAME_LENGTH		(128)
+# define COMMENT_LENGTH			(2048)
+# define COREWAR_EXEC_MAGIC		0xea83f3
+
+
+typedef struct		s_op
+{
+    char			*name;
+    int				arg_number;
+    char			arg_type[3];
+    int			    order;
+    int				duration;
+    char			*desc;
+    char			opcode;
+    char			dir_as_label;
+}					t_op;
+
+typedef struct		header_s
+{
+    unsigned int		magic;
+    char				prog_name[PROG_NAME_LENGTH + 1];
+    unsigned int		prog_size;
+    char				comment[COMMENT_LENGTH + 1];
+}					header_t;
+
+
+typedef struct		process_s
+{
+    char *name;
+    unsigned int registers[REG_NUMBER];
+    char carry;
+    unsigned int counter;
+    int color;
+    unsigned int live;
+    char *current_op;
+    int cycle_to_execute;
+}					t_process;
+
+typedef struct		player_s
+{
+    unsigned char *code;
+    unsigned int code_len;
+    char *player_name[PROG_NAME_LENGTH];
+    unsigned int player_number;
+    unsigned int last_live;
+    int color;
+}					t_player;
+
+
+
+
+typedef struct		env_s
+{
+    t_list *processes;
+    unsigned char global_field[MEM_SIZE];
+    int player_in_game;
+    unsigned int cycle;
+    int cycle_to_die;
+    t_list *players;
+
+}					t_env;
+
+extern void (*operation[16])(t_process *process);
+extern t_op g_tab[17];
+extern t_env *g_env;
+
+
+//comands
+
+void live(t_process *process);
+
+
+//operation_exec function
+
+int get_operation(t_process *process);
+void init_operation(int command, t_process *process);
+
+
+
+//ui functions
+void rewrite_memory(unsigned char *buff);
+void register_color_changes(int start, int len, int color);
+void init_screen();
+void dell_window();
+void place_cursor(t_process *process);
+
+//process function
+void add_new_process(int position, t_player *player);
+
+
+//envi functions
+void add_new_player(t_player *player);
+void init_env();
+void register_players(t_list *players);
+
+
+//cycle functions
+void start_cycle();
+
+//util
+int lst_size(t_list *list);
+void screen_cycle_status();
+
+#endif //COREWAR_OP_H
---
+++ ui.c
@@ -0,0 +1,122 @@
+#include "op.h"
+
+
+int mem_row_len = 64 * 3;
+int mem_col_len = MEM_SIZE / 64;
+
+static int colors[MEM_SIZE];
+static WINDOW *memory;
+static WINDOW *stat;
+
+WINDOW *create_memory_window()
+{
+
+    WINDOW *wraper = newwin(mem_col_len + 2, mem_row_len + 2, MARGIN, MARGIN);
+    WINDOW *memory = newwin(mem_col_len, mem_row_len, MARGIN + 1, MARGIN + 1);
+    wrefresh(wraper);
+    wrefresh(memory);
+    return memory;
+}
+
+WINDOW *create_stat_window()
+{
+
+    WINDOW *wrapper = newwin(mem_col_len + 2, 60, MARGIN, mem_row_len + 10);
+    WINDOW *stat = newwin(mem_col_len - 2, 60 - 2, MARGIN + 1, mem_row_len + 11);
+    wrefresh(wrapper);
+    wrefresh(stat);
+    return stat;
+}
+
+
+void register_color_changes(int start, int len, int color)
+{
+    int i = 0;
+
+    while(i < len)
+    {
+        colors[start + i] = color;
+        i++;
+    };
+}
+
+
+void rewrite_memory(unsigned char *buff)
+{
+    if (buff)
+        ;
+
+//        wmove(memory, 0, 0);
+//        int i = 0;
+//        while (i < MEM_SIZE)
+//        {
+//            if (colors[i] > 1000)
+//            {
+//                wattron(memory, COLOR_PAIR(colors[i] / 1000 + 1));
+//                wprintw(memory, "%.2X", buff[i]);
+//                wattroff(memory, COLOR_PAIR(colors[i] / 1000 + 1));
+//                wprintw(memory, " ");
+//                colors[i] = colors[i] % 1000;
+//            } else
+//            {
+//                wattron(memory, COLOR_PAIR(colors[i]));
+//                wprintw(memory, "%.2X", buff[i]);
+//                wattroff(memory, COLOR_PAIR(colors[i]));
+//                wprintw(memory, " ");
+//            }
+//            i++;
+//        }
+//        wrefresh(memory);
+}
+
+void init_def_color()
+{
+    int i = 0;
+
+    start_color();
+    init_pair(1, COLOR_CYAN,  COLOR_BLACK); // frame color
+    init_pair(2, COLOR_WHITE, COLOR_BLACK); // initial field color
+    init_pair(3, COLOR_GREEN, COLOR_BLACK); // player 1 color
+    init_pair(4, COLOR_BLACK, COLOR_GREEN); // player 1 cursor color
+    init_pair(5, COLOR_RED, COLOR_BLACK); // player 2 color
+    init_pair(6, COLOR_BLACK, COLOR_RED); // player 2 cursor color
+    init_pair(7, COLOR_MAGENTA, COLOR_BLACK); // player 3 color
+    init_pair(8, COLOR_BLACK, COLOR_MAGENTA); // player 3 cursor color
+    init_pair(9, COLOR_YELLOW, COLOR_BLACK); // player 4 color
+    init_pair(10, COLOR_BLACK, COLOR_YELLOW); // player 4 cursor color
+    init_pair(11, COLOR_BLUE, COLOR_BLACK); // player 5 color
+    init_pair(12, COLOR_BLACK, COLOR_BLUE); // player 5 cursor color
+    init_pair(13, COLOR_CYAN, COLOR_BLACK); // player 6 color
+    init_pair(14, COLOR_BLACK, COLOR_CYAN); // player 6 cursor color
+
+    while (i < MEM_SIZE)
+        colors[i++] = 2;
+}
+
+void init_screen()
+{
+    initscr();
+    signal (SIGWINCH, NULL);
+    init_def_color();
+    chtype background = '.' | COLOR_PAIR(1);
+    bkgd(background);
+    refresh();
+    memory = create_memory_window();
+    stat = create_stat_window();
+}
+
+void place_cursor(t_process *process)
+{
+
+    colors[process->counter] = process->color * 1000 + colors[process->counter];
+}
+
+void dell_window()
+{
+    delwin(memory);
+    delwin(stat);
+    endwin();
+}
+
+
+
