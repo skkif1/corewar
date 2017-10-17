706a4409767c72e89e402edaa7c3fd72a5e9bfe1
omotyliu <skkif1@gmail.com>
2017-10-17 15:30:07 +0300

    parser

---
+++ arg_parser.c
@@ -0,0 +1,13 @@
+#include "op.h"
+
+void parse_args(int argc, char **argv)
+{
+
+    if (argc > 1)
+    {
+        if (!ft_strcmp(argv[1], "-n"))
+            g_env->vis = 1;
+        else
+            g_env->vis = 0;
+    }
+}
