79a20f7d9e4fdda0395e589be3fb41ffc3c6d58c
omotyliu <skkif1@gmail.com>
2017-10-27 21:54:23 +0300

    and or xor

--- src/c_add.c
+++ src/c_add.c
@@ -34,7 +34,10 @@ void add(t_process *process)
 	r3 = g_env->global_field[process->counter + 4];
 	if(!(validate_reqistry(r1) && validate_reqistry(r2) && validate_reqistry(r3)))
 		return;
-	process->registers[r3] = process->registers[r1] + process->registers[r2];
+	if(get_opcode_by_name(process->current_op) == 4)
+		process->registers[r3] = process->registers[r1] + process->registers[r2];
+	else
+		process->registers[r3] = process->registers[r1] - process->registers[r2];
 	process->counter += 5;
 	process->carry = (char) ((process->registers[r3] == 0) ? 1 : 0);
 }
--- src/c_and.c
+++ src/c_and.c
@@ -14,35 +14,57 @@ unsigned int make_op(t_process *process, unsigned int first, unsigned int second
 	return res;
 }
 
+int validate_and(int *mass, t_process *process)
+{
+	int i;
+
+	i = 0;
+	if((mass[0] == T_REG || mass[0] == T_DIR|| mass[0] == T_IND) &&
+			(mass[1] == T_REG || mass[1] == T_DIR|| mass[1] == T_IND) &&
+			(mass[2] == T_REG))
+		i = 1;
+	type_to_size(mass);
+	if(!i)
+	{
+		invalid_coding_byte(mass, process);
+	}
+	return i;
+}
+
+unsigned int get_arg_and(int *params, int num, t_process *process)
+{
+	unsigned int arg;
+
+	if(num == 0)
+		arg = bytes_to_int(&g_env->global_field[process->counter + 2], params[0]);
+	else
+		arg = bytes_to_int(&g_env->global_field[process->counter + 2 + params[0]], params[1]);
+
+	if (params[num] == IND_SIZE || params[num] == 1)
+		arg = (params[num] == IND_SIZE) ? bytes_to_int(&g_env->global_field[arg], DIR_SIZE) : process->registers[arg];
+	return arg;
+}
+
+
 void and(t_process *process)
 {
 	char coding_byte;
 	unsigned int first;
 	unsigned int second;
-	unsigned int regist;
-	unsigned int value;
 	int params[3];
 
 	coding_byte = g_env->global_field[process->counter + 1];
 	get_arg_types(params, coding_byte);
-	if (check_param(params))
-	{
-		///bad params
-	}
-	type_to_size(params);
 
-	first = bytes_to_int(&g_env->global_field[process->counter + 2], params[0]);
-	second = bytes_to_int(&g_env->global_field[process->counter + 2 + params[0]], params[1]);
-
-	if (params[0] == IND_SIZE || params[0] == 1)
-		first = (params[0] == IND_SIZE) ? bytes_to_int(&g_env->global_field[first], DIR_SIZE) : process->registers[first];
-	if(params[1] == IND_SIZE || params[1] == 1)
-		second = (params[0] == IND_SIZE) ? bytes_to_int(&g_env->global_field[second], DIR_SIZE) : process->registers[second];
-	regist = g_env->global_field[process->counter + 2 + params[0] + params[1]];
-	value = make_op(process, first, second);
-	process->registers[regist] = value;
-	if (value == 0)
-		process->carry = 1;
+	if (!validate_and(params, process))
+		return;
+	first = get_arg_and(params, 0, process);
+	second = get_arg_and(params, 1, process);
+	coding_byte = g_env->global_field[process->counter + 2 + params[0] + params[1]];
+	if(!validate_reqistry(coding_byte))
+		return;
+	process->registers[(int)coding_byte] = make_op(process, first, second);
+	process->carry = (char) ((process->registers[(int)coding_byte] == 0) ? 1 : 0);
 	process->counter += params[0] + params[1] + params[2] + 2;
 }
 
