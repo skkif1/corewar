cee6ec3f284a8a0beec91e63a5f06ada0dfe7e99
omotyliu <skkif1@gmail.com>
2017-10-29 18:20:56 +0200

    last working

--- src/environment.c
+++ src/environment.c
@@ -1,44 +1,5 @@
 #include "../includes/op.h"
 
-t_env *g_env;
-
-static int	ap_basecheck(char c, int base)
-{
-	if (base <= 10)
-		return (c >= '0' && c <= ('9' + base - 10));
-	return ((c >= '0' && c <= '9') ||
-			(c >= 'A' && c <= ('A' + base - 10)) ||
-			(c >= 'a' && c <= ('a' + base - 10)));
-}
-
-int			ft_atoi_base(const char *str, int str_base)
-{
-	int i;
-	int	sign;
-	int atoi;
-
-	i = 0;
-	atoi = 0;
-	if (str_base < 2 || str_base > 16)
-		return (0);
-	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
-		i++;
-	sign = (str[i] == '-') ? -1 : 1;
-	if (str[i] == '+' || str[i] == '-')
-		i++;
-	while (ap_basecheck(str[i], str_base))
-	{
-		if (str[i] - 'a' >= 0)
-			atoi = atoi * str_base + (str[i] - 'a' + 10);
-		else if (str[i] - 'A' >= 0)
-			atoi = atoi * str_base + (str[i] - 'A' + 10);
-		else
-			atoi = atoi * str_base + (str[i] - '0');
-		i++;
-	}
-	return (sign * atoi);
-}
-
 void		reverse_bytes(unsigned int *bytes, int size)
 {
 	unsigned char	*save;
--- src/vm.c
+++ src/vm.c
@@ -4,17 +4,17 @@ t_env *g_env;
 
 int main(int argc, char **argv)
 {
+	t_list *players = NULL;
+
+
     init_env();
     parse_args(argc, argv);
 
     if (g_env->vis)
         init_screen();
-
-    t_list *players = NULL;
-//    register_players(players);
     register_players_auto(players);
     start_cycle();
-
+	
     if(g_env->vis)
     {
         getch();
