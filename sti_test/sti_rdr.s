22d3cdfb2903d6621270d11633c1fd8ae5edabf9
Vitaliy Rudakov <vrudakov@e1r7p4.unit.ua>
2018-01-14 17:14:08 +0200

    sti_rdr

--- sti_test/sti_rdr.s
+++ sti_test/sti_rdr.s
@@ -1,35 +1,35 @@
 .name "Mat"
 .comment "Mat"
 
-	ld	r2, %100
+	ld %100, r2
 	sti r1, %100, r2
 
-	ld	r2, 
-	sti r1, %100, %32767
+	ld %32767, r2
+	sti r1, %100, r2
 
-	ld	r2, %200
+	ld %200, r2
 	sti r1, %-100, r2
 
-	ld	r2, %-300
+	ld %-300, r2
 	sti r1, %100, r2
 
-	ld	r2, %-200
+	ld %-200, r2
 	sti r1, %-200, r2
 
-	ld	r2, %32769
+	ld %32769, r2
 	sti r1, %100, r2 
 
-	ld	r2, %100
+	ld %100, r2
 	sti r1, %32769, r2
 
-	ld	r2, %3000
+	ld %3000, r2
 	sti r1, %3000, r2
 
-	ld	r2, %-3000
+	ld %-3000, r2
 	sti r1, %-3000, r2
 
-	ld	r2, %32767
+	ld %32767, r2
 	sti r1, %100, r2
 
-	ld	r2, %500000000	
+	ld %500000000, r2
 	sti r1, %2000000000, r2
